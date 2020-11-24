47b28caaaf6072acb977e1e68ab350d80417fc47
wzl9988 <55666221+wzl9988@users.noreply.github.com>
2020-11-24 20:39:03 +0800

    Add files via upload

---
+++ 112402.回车键判断App.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class app {
+    JPanel myPanel=new JPanel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+    public app() {
+        textArea_A.setBounds(100,100,256,128);
+        textArea_A.setBackground(Color.orange);
+        myPanel.add(textArea_A);
+        textArea_B.setBounds(100,300,256,128);
+        textArea_B.setBackground(Color.yellow);
+        myPanel.add(textArea_B);
+
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    if (textArea_A.getText().equals("WZL")){
+                        textArea_B.setText("正确");
+                    }
+                else {
+
+                    textArea_B.setText("错误");
+                }}
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    textArea_B.setText("");
+                }
+            }
+        });
+    }
+    //显示窗体方法
+    void go(){
+        JFrame frame = new JFrame("窗体123456");
+        myPanel.setLayout(null);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,512,720);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new app().go();
+    }
+}
