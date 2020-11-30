07b619261cb8a5c2f01dec2202707ef4019f6e81
wzl9988 <55666221+wzl9988@users.noreply.github.com>
2020-11-30 15:07:22 +0800

    Add files via upload

---
+++ 113001.子类Ap.java
@@ -0,0 +1,7 @@
+public class ap extends R {
+	public static void main(String[] args) {
+		System.out.println(boy_filePath);
+		System.out.println(girl_filePath);
+		System.out.println(array2d[0][1]);
+	}
+}
