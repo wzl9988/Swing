03e70c230fad850e89816f70f0b47d9c40015665
wzl9988 <55666221+wzl9988@users.noreply.github.com>
2020-11-30 15:11:22 +0800

    接口

---
+++ 113002实现接口ap.java
@@ -0,0 +1,7 @@
+public class ap implements R {
+	public static void main(String[] args) {
+		System.out.println(R.boy_filePath);
+		System.out.println(R.girl_filePath);
+		System.out.println(R.array2d[0][1]);
+	}
+}
